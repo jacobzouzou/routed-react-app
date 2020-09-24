1218c9e759561e0e67b92e6ed6f12dc3bd39c390
jacobzouzou <jacob.zouzou@gmail.com>
2020-09-24 20:57:54 +0200

    add test files

--- src/Tests/spnashot_with_renderer.test.js
+++ src/Tests/spnashot_with_renderer.test.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { create,act } from "react-test-renderer";
+import { create,act} from "react-test-renderer";
+
 import App from "../App";
 
-// test("renders learn react link", () => {
+// test("renders learn react link", () => {a
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/Testing/);
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-//snapthot with renderer
+//snapshot with renderer
 it("renders correctly", () => {
   let root;
   act(() => {
