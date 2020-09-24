7154b4065385d6fdccdff1c0318b1fd045e34751
jacobzouzou <jacob.zouzou@gmail.com>
2020-09-24 22:14:05 +0200

    update

--- src/Tests/mocking.package.test.js
+++
@@ -1,9 +0,0 @@
-// const mathjs = require("../../__mocks__/maths")
-import mathjs from "../../__mocks__/maths";
-
-test(`The mathjs sub function`, () => {
-  const result = mathjs.sub(10000, 1000);
-  expect(result).toBe(9000);
-  expect(mathjs.sub).toHaveBeenCalled();
-  expect(mathjs.sub).toHaveBeenCalledWith(10000, 1000);
-});
